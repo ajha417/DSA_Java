6f25324fb3f33098ad38d83060ff748c25f5240d
ajha417 <ajha417@rku.ac.in>
2023-05-07 18:43:38 +0530

    adds writeintofiles

--- WriteIntoFile.java
+++ WriteIntoFile.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class WriteIntoFile {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteIntoFile {
+    public static void main(String[] args) {
+        try{
+//            FileWriter fw = new FileWriter("newfile.txt");
+            BufferedWriter fw = new BufferedWriter(new FileWriter("newfile.txt",true));
+            fw.write("This is the content written in file");
+            fw.close();
+            System.out.println("content written successfully");
+        }
+        catch (IOException ex){
+            System.out.println("something went wrong");
+            ex.printStackTrace();
+        }
+    }
 }
