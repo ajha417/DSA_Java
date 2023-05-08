ed92dc3bb0eb080ab3a1de867670ab6e02146c14
ajha417 <ajha417@rku.ac.in>
2023-05-08 08:56:17 +0530

    updates jdbc file

--- JDBC_Example.java
+++ JDBC_Example.java
@@ -7,11 +7,29 @@ public class JDBC_Example {
             String uname = "root";
             String pass = "";
             Connection conn = DriverManager.getConnection(url,uname,pass);
-            Statement st = conn.createStatement();
-            ResultSet res = st.executeQuery("SELECT * FROM users");
-            while (res.next()){
+
+//            to insert single value
+           /* Statement st = conn.createStatement();
+//            ResultSet res = st.executeQuery("SELECT * FROM users");
+         *//*   while (res.next()){
                 System.out.println(res.getString("name")+" "+res.getString("address"));
-            }
+            }*//*
+
+//            to insert value
+            int count = st.executeUpdate("INSERT INTO users VALUES(4,'Hugh JackMan','California')");
+*/
+
+
+//            to insert multiple value
+            String query = "INSERT INTO users VALUES(?,?,?)";
+
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setInt(1,11);
+            st.setString(2,"Jeff Bezzos");
+            st.setString(3,"New york");
+            int count = st.executeUpdate();
+            System.out.println(count + " row/s affected");
+
             st.close();
             conn.close();
         }
