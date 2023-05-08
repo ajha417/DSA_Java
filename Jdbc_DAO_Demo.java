b89bcf058d60a51ea0b0969bbe6b228b58dc09cc
ajha417 <ajha417@rku.ac.in>
2023-05-08 09:49:26 +0530

    implements addstudent method using dao patter

--- Jdbc_DAO_Demo.java
+++ Jdbc_DAO_Demo.java
@@ -1,24 +1,42 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Jdbc_DAO_Demo {
     public static void main(String[] args) {
         StudentDAO dao = new StudentDAO();
-        Studentss s1 = dao.getStudent(11);
-        System.out.println(s1.sname);
+
+//        to get the value
+       /* Studentss s1 = dao.getStudent(11);
+        System.out.println(s1.sname);*/
+
+//        to add student
+        Studentss s2 = new Studentss();
+        s2.rollno = 12;
+        s2.sname = "Hrithik";
+        s2.address = "Mumbai";
+        dao.connect();
+        dao.addStudent(s2);
     }
 }
 
 class StudentDAO{
-    public Studentss getStudent(int rollno){
+    Connection conn = null;
+    public void connect(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+             conn  = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root","");
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public Studentss getStudent(int rollno) throws SQLException {
         try {
             String query = "SELECT name FROM users WHERE id="+rollno;
             Studentss s = new Studentss();
             s.rollno = rollno;
             Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root","");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root","");
             Statement st = conn.createStatement();
             ResultSet res = st.executeQuery(query);
             res.next();
@@ -29,10 +47,29 @@ class StudentDAO{
         catch (Exception ex){
             System.out.println(ex.getMessage());
         }
+        finally {
+            conn.close();
+        }
         return null;
+    }
+
+    public void addStudent(Studentss s){
+        String query = "INSERT INTO users VALUES(?,?,?)";
+        PreparedStatement pst;
+        try{
+            pst = conn.prepareStatement(query);
+            pst.setInt(1,s.rollno);
+            pst.setString(2,s.sname);
+            pst.setString(3,s.address);
+            pst.executeUpdate();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 }
 class Studentss{
     int rollno;
-    String sname;
+    String sname,address;
 }
