50a1427f8e995a4b6c85603a3ffe1a87c9cc58e9
ajha417 <ajha417@rku.ac.in>
2025-09-19 08:55:46 +0530

    adds program to move all zeroes to end of the array

---
+++ com/array/MoveZeroToEnd.java
@@ -0,0 +1,29 @@
+package com.array;
+
+public class MoveZeroToEnd {
+    public static void main(String[] args) {
+        int []arr = new int[]{1,0,2,0,3,4,0};
+        moveAllZerosToEnd(arr);
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+    private static void moveAllZerosToEnd(int[] arr) {
+        int j = -1;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+        if (j == -1) return;
+        for(int i = j + 1; i < arr.length; i++) {
+            if(arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+}
