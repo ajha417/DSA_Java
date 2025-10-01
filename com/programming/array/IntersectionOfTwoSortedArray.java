a87b4c89d809233e2ec2658428e6712e0cce19c3
ajha417 <ajha417@rku.ac.in>
2025-10-01 09:11:39 +0530

    optimal approach for IntersectionOfTwoSortedArray

--- com/programming/array/IntersectionOfTwoSortedArray.java
+++ com/programming/array/IntersectionOfTwoSortedArray.java
@@ -14,15 +14,29 @@ public class IntersectionOfTwoSortedArray {
     private static List<Integer> findIntersectionOfTwoSortedArray(int[] arr1, int[] arr2, int n, int m) {
         List<Integer> ans = new ArrayList<>();
         // This is the brute force approach
-        boolean[] visited = new boolean[m];
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(arr1[i] == arr2[j] && !visited[j]) {
-                    ans.add(arr1[i]);
-                    visited[j] = true;
-                    break;
-                }
-                if (arr2[j] > arr1[i]) break;
+//        boolean[] visited = new boolean[m];
+//        for(int i = 0; i < n; i++) {
+//            for(int j = 0; j < m; j++) {
+//                if(arr1[i] == arr2[j] && !visited[j]) {
+//                    ans.add(arr1[i]);
+//                    visited[j] = true;
+//                    break;
+//                }
+//                if (arr2[j] > arr1[i]) break;
+//            }
+//        }
+
+        //optimal approach
+        int i = 0, j = 0;
+        while(i < n && j < m) {
+            if(arr1[i] < arr2[j]) {
+                i++;
+            } else if(arr1[i] > arr2[j]) {
+                j++;
+            } else {
+                ans.add(arr1[i]);
+                i++;
+                j++;
             }
         }
         return ans;
