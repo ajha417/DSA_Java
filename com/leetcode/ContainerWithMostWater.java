a7aca98ed9d547d7861d1deb0fcdc794002a261b
ajha417 <ajha417@rku.ac.in>
2026-01-31 14:28:18 +0530

    adds program to find the max water stored between the towers

---
+++ com/leetcode/ContainerWithMostWater.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        int maxWaterStored = findMaxWater(height);
+        System.out.println(maxWaterStored);
+    }
+
+    private static int findMaxWater(int[] height) {
+
+        int max = -1;
+//        the time complexity here will be O(N*N)
+//        for(int i = 0; i < height.length; i++) {
+//            for(int j = i + 1; j < height.length; j++) {
+//                int minHeight = Math.min(height[i], height[j]);
+//                int waterStored = minHeight * (j - i);
+//                max = Math.max(max, waterStored);
+//            }
+//        }
+
+//        Optimized approach
+        int right = height.length - 1;
+        int left = 0;
+        while(left < right) {
+            int leftHeight = height[left];
+            int rightHeight = height[right];
+            int minHeight = Math.min(leftHeight, rightHeight);
+            int waterStored = minHeight * (right - left);
+            max = Math.max(max, waterStored);
+            if(leftHeight < rightHeight) left++;
+            else right--;
+        }
+        return max;
+    }
+}
