e61178b5b5232b63a0ff6a2e1a99be94fb462c83
ajha417 <ajha417@rku.ac.in>
2026-02-01 10:41:42 +0530

    adds program to traverse binary level order traversal

---
+++ com/leetcode/prep/tree/BinaryLevelOrderTraversal.java
@@ -0,0 +1,61 @@
+package com.leetcode.prep.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryLevelOrderTraversal {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        root.left.left.left = null;
+        root.left.left.right = null;
+
+        root.right.right.left = null;
+        root.right.right.right = null;
+
+        List<List<Integer>> result = levelOrderTraversal(root);
+        System.out.println(result);
+    }
+
+    private static List<List<Integer>> levelOrderTraversal(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
