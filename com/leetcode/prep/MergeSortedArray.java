ceb427e3d0f221b2c30626c6994de7aafd038636
ajha417 <ajha417@rku.ac.in>
2026-02-01 22:41:07 +0530

    merge two sorted array

---
+++ com/leetcode/prep/MergeSortedArray.java
@@ -0,0 +1,31 @@
+package com.leetcode.prep;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        int m = 3;
+        int n = 3;
+        merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    private static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while(i >= 0) {
+            nums1[k--] = nums1[i--];
+        }
+        while(j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
