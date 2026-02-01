a100251629b1802644d40df9ac1c65257887d917
ajha417 <ajha417@rku.ac.in>
2026-02-01 21:56:45 +0530

    adds program to find the number of islands from the given grid

---
+++ com/leetcode/prep/graph/NumberOfIslands.java
@@ -0,0 +1,37 @@
+package com.leetcode.prep.graph;
+
+public class NumberOfIslands {
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+
+        int result = findNumberOfIsland(grid);
+        System.out.println(result);
+    }
+
+    private static int findNumberOfIsland(char[][] grid) {
+        int count = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if(grid[i][0] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private static void dfs(char[][] grid, int i, int j) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][0] == '0') return;
+        grid[i][0] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+}
