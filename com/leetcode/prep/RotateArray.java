de1bd28d717f79b05aa2b3ae0c8d9b8f7cf600b4
ajha417 <ajha417@rku.ac.in>
2026-02-02 23:36:51 +0530

    minor change in rotate array program

--- com/leetcode/prep/RotateArray.java
+++ com/leetcode/prep/RotateArray.java
@@ -12,10 +12,22 @@ public class RotateArray {
     private static void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
-        for(int i = 0; i < k; i++) {
-            int temp = nums[n - (k - i)];
-            nums[n - (k - i)] = nums[i];
-            nums[i] = temp;
+//        for(int i = 0; i < k; i++) {
+//            int temp = nums[n - (k - i)];
+//            nums[n - (k - i)] = nums[i];
+//            nums[i] = temp;
+//        }
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++; end--;
         }
     }
 }
